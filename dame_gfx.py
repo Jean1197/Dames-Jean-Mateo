f65da2c8b9464093ee6532af9df198eb104b2c67
MateoGrgic <pm05qog@eduvaud.ch>
2024-12-05 12:09:10 +0100

    Imports

--- dame_gfx.py
+++ dame_gfx.py
@@ -3,3 +3,7 @@ Nom    : dame_gfx.py
 Auteur : Jean-Christophe Serrano x Mateo Grgic
 Date   : 05.12.2024
 '''
+
+import pygame
+import dame_rules as rules
+
--- dame_rules.py
+++ dame_rules.py
@@ -3,3 +3,4 @@ Nom    : dame_rules.py
 Auteur : Jean-Christophe Serrano x Mateo Grgic
 Date   : 05.12.2024
 '''
+
