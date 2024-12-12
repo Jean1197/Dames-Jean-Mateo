27405685ad55c3fbf4bd5e3a3203cacd5399606e
MateoGrgic <pm05qog@eduvaud.ch>
2024-12-12 12:14:02 +0100

    affichage de tout les pions

--- dame_gfx.py
+++ dame_gfx.py
@@ -6,19 +6,45 @@ Date   : 05.12.2024
 
 import pygame
 
-
 def dessine_plateau(screen, nb_lignes, nb_colonnes, case_size, cases_blanches, cases_noires):
+    """Dessine le plateau de jeu avec alternance des couleurs."""
     for ligne in range(nb_lignes):
         for colonne in range(nb_colonnes):
             couleur = cases_blanches if (ligne + colonne) % 2 == 0 else cases_noires
             pygame.draw.rect(screen, couleur, (colonne * case_size, ligne * case_size, case_size, case_size))
 
-
 def charger_images(case_size):
-    pion = pygame.image.load("MA-24_pion_noir.png")
-    pion = pygame.transform.scale(pion, (case_size, case_size))
+    """Charge et redimensionne les images des pions."""
+    pion_noir = pygame.image.load("MA-24_pion_noir.png")
+    pion_noir = pygame.transform.scale(pion_noir, (case_size, case_size))
 
-    pion1 = pygame.image.load("MA-24_pion.png")
-    pion1 = pygame.transform.scale(pion1, (case_size, case_size))
+    pion_blanc = pygame.image.load("MA-24_pion.png")
+    pion_blanc = pygame.transform.scale(pion_blanc, (case_size, case_size))
 
-    return pion, pion1
+    return pion_noir, pion_blanc
+
+def initialiser_pions(nb_lignes, nb_colonnes):
+    """Initialise les positions des pions noirs et blancs."""
+    pions_noirs = []
+    pions_blancs = []
+
+    # Ajouter les pions noirs dans les trois premières lignes sur les cases noires
+    for ligne in range(3):
+        for colonne in range(nb_colonnes):
+            if (ligne + colonne) % 2 == 1:  # Case noire
+                pions_noirs.append((ligne, colonne))
+
+    # Ajouter les pions blancs dans les trois dernières lignes sur les cases noires
+    for ligne in range(nb_lignes - 3, nb_lignes):
+        for colonne in range(nb_colonnes):
+            if (ligne + colonne) % 2 == 1:  # Case noire
+                pions_blancs.append((ligne, colonne))
+
+    return pions_noirs, pions_blancs
+
+def afficher_pions(screen, pions, pion_image, case_size):
+    """Affiche les pions sur le plateau."""
+    for ligne, colonne in pions:
+        x = colonne * case_size
+        y = ligne * case_size
+        screen.blit(pion_image, (x, y))
--- dame_main.py
+++ dame_main.py
@@ -6,8 +6,7 @@ Date   : 22.11.2024
 
 import pygame
 import sys
-from dame_gfx import dessine_plateau, charger_images
-from dame_rules import bouge_bas_droite, bouge_bas_gauche, bouge_haut_droite, bouge_haut_gauche
+from dame_gfx import dessine_plateau, charger_images, initialiser_pions, afficher_pions
 
 # Paramètres du plateau
 case_size = 50
@@ -15,43 +14,33 @@ cases_blanches = (200, 173, 127)
 cases_noires = (91, 60, 17)
 nb_lignes, nb_colonnes = 10, 10
 
-# Position initiale des pions
-pion_col, pion_ligne = 1, 0
-pion_col1, pion_ligne1 = 2, 9
-
 # Initialisation de pygame
 pygame.init()
 screen = pygame.display.set_mode((nb_colonnes * case_size, nb_lignes * case_size))
 pygame.display.set_caption("Jeu de dames")
 
 # Charger les images des pions
-pion, pion1 = charger_images(case_size)
+pion_noir, pion_blanc = charger_images(case_size)
+
+# Initialiser les positions des pions
+pions_noirs, pions_blancs = initialiser_pions(nb_lignes, nb_colonnes)
 
 # Boucle principale
 def main():
-    global pion_col, pion_ligne, pion_col1, pion_ligne1
     running = True
     while running:
         # Dessiner le plateau
         dessine_plateau(screen, nb_lignes, nb_colonnes, case_size, cases_blanches, cases_noires)
 
-        # Afficher les pions
-        screen.blit(pion, (pion_col * case_size, pion_ligne * case_size))
-        screen.blit(pion1, (pion_col1 * case_size, pion_ligne1 * case_size))
+        # Afficher les pions noirs et blancs
+        afficher_pions(screen, pions_noirs, pion_noir, case_size)
+        afficher_pions(screen, pions_blancs, pion_blanc, case_size)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    pion_col, pion_ligne = bouge_bas_droite(pion_col, pion_ligne, nb_colonnes, nb_lignes)
-                elif event.key == pygame.K_LEFT:
-                    pion_col, pion_ligne = bouge_bas_gauche(pion_col, pion_ligne, nb_colonnes, nb_lignes)
-                elif event.key == pygame.K_UP:
-                    pion_col1, pion_ligne1 = bouge_haut_droite(pion_col1, pion_ligne1, nb_colonnes, nb_lignes)
-                elif event.key == pygame.K_DOWN:
-                    pion_col1, pion_ligne1 = bouge_haut_gauche(pion_col1, pion_ligne1, nb_colonnes, nb_lignes)
-                elif event.key == pygame.K_q:
+                if event.key == pygame.K_q:
                     running = False
 
         pygame.display.flip()
